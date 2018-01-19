302d2d051f541d21e0e7260abfd2d1485e8908fe
t4rn <kwitkowski.it@gmail.com>
2018-01-19 08:42:28 +0100

    Batch file - copy config

--- gugleus_dev_publish.bat
+++ gugleus_dev_publish.bat
@@ -5,6 +5,8 @@ set pool="gugleus_dev"
 set solutionFolder="C:\Projekty\_kw\Gugleus\"
 set source="C:\Projekty\_kw\Gugleus\Gugleus.Api"
 set dest="c:\inetpub\wwwroot\gugleus_dev"
+set configSource="C:\inetpub\wwwroot\appsettings_dev.json"
+set configDest= "%dest%\appsettings.json"
 @echo on
 
 @REM stop application pool in IIS
@@ -17,6 +19,9 @@ git pull
 @REM publish to inetpub
 cd %source%
 dotnet publish -o %dest%
+
+@REM copy config
+copy /y %configSource% %configDest%
 
 @REM start application pool in IIS
 %SYSTEMROOT%\System32\inetsrv\appcmd start apppool /apppool.name:%pool%
