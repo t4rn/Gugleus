76a443ae0c4ccc06e1ce414baf6e968863534355
t4rn <kwitkowski.it@gmail.com>
2018-03-16 16:03:12 +0100

    ErrorHandlingMiddleware

---
+++ Gugleus.Api/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Gugleus.Api.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            HttpStatusCode code = HttpStatusCode.ServiceUnavailable;
+
+            context.Response.ContentType = "text/plain";// "application/json";
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(exception.Message);
+        }
+    }
+}
