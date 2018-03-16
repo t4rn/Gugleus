1055d4b10e1f65bc967898ef2a3e9ebe88abc770
t4rn <kwitkowski.it@gmail.com>
2018-03-16 21:35:12 +0100

    WebUI - show errors

--- Gugleus.WebUI/Models/Requests/RequestVM.cs
+++ Gugleus.WebUI/Models/Requests/RequestVM.cs
@@ -1,4 +1,5 @@
 ﻿using Gugleus.Core.Domain;
+using Gugleus.Core.Domain.Dictionaries;
 using Gugleus.Core.Extensions;
 using System;
 
@@ -16,6 +17,20 @@ namespace Gugleus.WebUI.Models.Requests
         public RequestQueueVM Queue { get; set; }
         public EnvType? Env { get; internal set; }
 
+
+        /// <summary>
+        /// Color for background on Request List table
+        /// </summary>
+        public string BackgroundColor
+        {
+            get
+            {
+                // if Error -> pale red
+                return Queue?.Status == RequestStatus.RequestStatusCode.ERR.ToString() ?
+                    "#ffb3b3" : "";
+            }
+        }
+
         public string InputShort(int chars)
         {
             if (!string.IsNullOrWhiteSpace(Input))
@@ -26,10 +41,23 @@ namespace Gugleus.WebUI.Models.Requests
 
         public string OutputShort(int chars)
         {
-            if (!string.IsNullOrWhiteSpace(Output))
-                return Output.Shorten(chars);
+            string retVal = string.Empty;
+            if (Queue?.Status == RequestStatus.RequestStatusCode.ERR.ToString() &&
+                !string.IsNullOrWhiteSpace(Queue?.ErrorMsg))
+            {
+                // on Error status show Error
+                retVal = Queue.ErrorMsg.Shorten(chars);
+            }
+            else if (!string.IsNullOrWhiteSpace(Output))
+            {
+                // on other statuses show Output
+                retVal = Output.Shorten(chars);
+            }
 
-            return string.Empty;
+            //if (!string.IsNullOrWhiteSpace(Output))
+            //    return Output.Shorten(chars);
+
+            return retVal;
         }
     }
 }
